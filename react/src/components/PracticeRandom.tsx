73402c96ec2b51efb307ea708279bddfecc33c6d
brownrice55 <113149599+brownrice55@users.noreply.github.com>
2025-07-21 10:20:43 +0900

    feat: add functions related to SpeechSynthesisUtterance

--- react/src/components/PracticeRandom.tsx
+++ react/src/components/PracticeRandom.tsx
@@ -10,10 +10,12 @@ import VolumeIcons from "./VolumeIcons";
 type PracticeRandomProps = {
   data: Map<number, Inputs>;
   randomIndexArray: number[];
+  lang: string[];
 };
 export default function PracticeRandom({
   data,
   randomIndexArray,
+  lang,
 }: PracticeRandomProps) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const currentKey: number = randomIndexArray[currentIndex];
@@ -38,10 +40,33 @@ export default function PracticeRandom({
   };
 
   const handleAudioOnOff = (e: any) => {
-    const index = e.currentTarget.dataset.key;
+    const index: number = parseInt(e.currentTarget.dataset.key);
     const newArray = Array(answersLength).fill(false);
     newArray[index] = !volumeIconsOn[index];
     setVolumeIconsOn(newArray);
+    if (newArray[index]) {
+      const text =
+        index === 0
+          ? displayData?.question
+          : index === 1
+          ? displayData?.answer
+          : displayData?.answers[index - 2].answer;
+      const voice = new SpeechSynthesisUtterance(text);
+      voice.lang = index === 0 ? lang[0] : lang[1];
+      voice.rate = 0.5;
+      speechSynthesis.cancel();
+      speechSynthesis.speak(voice);
+
+      voice.onend = () => {
+        setVolumeIconsOn((prev) => {
+          const newArray = [...prev];
+          newArray[index] = false;
+          return newArray;
+        });
+      };
+    } else {
+      speechSynthesis.cancel();
+    }
   };
 
   return (
@@ -115,6 +140,7 @@ export default function PracticeRandom({
           <>
             <div className="px-3 py-1 bg-light">
               <div className="p-2 fs-3 fw-light">
+                <p className="fs-6 m-0">回答例1</p>
                 <span className="pe-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -140,33 +166,38 @@ export default function PracticeRandom({
               </div>
 
               {displayData?.answers.map((val, index) => (
-                <div className="p-2 fs-3 fw-light" key={index + 2}>
-                  <span className="pe-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      className="bi bi-chat-text"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                      <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
-                    </svg>
-                  </span>
-                  {val.answer}
-                  <Button
-                    variant="light"
-                    className="p-1 ms-2"
-                    data-key={index + 2}
-                    onClick={(e) => handleAudioOnOff(e)}
-                  >
-                    <VolumeIcons
-                      volumeIconsOn={volumeIconsOn}
-                      index={index + 2}
-                    />
-                  </Button>
-                </div>
+                <>
+                  {val.answer && (
+                    <div className="p-2 fs-3 fw-light" key={index + 2}>
+                      <p className="fs-6 m-0">回答例{index + 2}</p>
+                      <span className="pe-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          fill="currentColor"
+                          className="bi bi-chat-text"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
+                          <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
+                        </svg>
+                      </span>
+                      {val.answer}
+                      <Button
+                        variant="light"
+                        className="p-1 ms-2"
+                        data-key={index + 2}
+                        onClick={(e) => handleAudioOnOff(e)}
+                      >
+                        <VolumeIcons
+                          volumeIconsOn={volumeIconsOn}
+                          index={index + 2}
+                        />
+                      </Button>
+                    </div>
+                  )}
+                </>
               ))}
             </div>
 
--- react/src/pages/Practice.tsx
+++ react/src/pages/Practice.tsx
@@ -2,11 +2,12 @@ import { useMemo } from "react";
 import Nav from "react-bootstrap/Nav";
 import Header from "../components/Header";
 import PracticeRandom from "../components/PracticeRandom";
-import { getData, getRandomIndexArray } from "../utils/common";
+import { getData, getRandomIndexArray, getAudio } from "../utils/common";
 
 export default function Practice() {
   const data = getData();
   const randomIndexArray = useMemo(() => getRandomIndexArray(data), []);
+  const lang = getAudio();
 
   return (
     <>
@@ -26,7 +27,11 @@ export default function Practice() {
           <Nav.Link eventKey="link-2">リスト</Nav.Link>
         </Nav.Item>
       </Nav>
-      <PracticeRandom data={data} randomIndexArray={randomIndexArray} />
+      <PracticeRandom
+        data={data}
+        randomIndexArray={randomIndexArray}
+        lang={lang}
+      />
     </>
   );
 }
